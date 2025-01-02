6ad5efc4fb56a0cf46468ae94ea70da3ed03665b
bookpoint54354 <bookpoint78@gmail.com>
2025-01-02 07:11:07 +0500

    Update finetune_cli.py

--- src/f5_tts/train/finetune_cli.py
+++ src/f5_tts/train/finetune_cli.py
@@ -87,10 +87,10 @@ def main():
     if args.exp_name == "F5TTS_Base":
         wandb_resume_id = None
         model_cls = DiT
-        model_cfg = dict(dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, conv_layers=4)
+        model_cfg = dict(dim=768, depth=18, heads=12, ff_mult=2, text_dim=512, conv_layers=4)
         if args.finetune:
             if args.pretrain is None:
-                ckpt_path = str(cached_path("hf://SWivid/F5-TTS/F5TTS_Base/model_1200000.pt"))
+                ckpt_path = str(cached_path("hf://SPRINGLab/F5-Hindi-24KHz/model_2500000.pt"))
             else:
                 ckpt_path = args.pretrain
     elif args.exp_name == "E2TTS_Base":
