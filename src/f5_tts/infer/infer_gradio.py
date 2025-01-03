f3714319d991a463989bc4d4a2e736117d2ba0f4
bookpoint54354 <bookpoint78@gmail.com>
2025-01-03 17:14:19 +0500

    Update infer_gradio.py

--- src/f5_tts/infer/infer_gradio.py
+++ src/f5_tts/infer/infer_gradio.py
@@ -40,7 +40,7 @@ from f5_tts.infer.utils_infer import (
 )
 
 
-DEFAULT_TTS_MODEL = "F5-TTS-small"
+DEFAULT_TTS_MODEL = "F5-TTS"
 tts_model_choice = DEFAULT_TTS_MODEL
 
 
@@ -53,6 +53,14 @@ def load_f5tts_small(ckpt_path="/kaggle/working/f5-hin/ckpts/f5/model_2529747.pt
     F5TTS_small_model_cfg = dict(dim=768, depth=18, heads=12, ff_mult=2, text_dim=512, conv_layers=4)
     return load_model(DiT, F5TTS_small_model_cfg, ckpt_path, vocab_file=vocab_path)
 
+def load_f5tts(ckpt_path=str(cached_path("hf://SWivid/F5-TTS/F5TTS_Base/model_1200000.safetensors"))):
+    F5TTS_model_cfg = dict(dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, conv_layers=4)
+    return load_model(DiT, F5TTS_model_cfg, ckpt_path)
+
+
+def load_e2tts(ckpt_path=str(cached_path("hf://SWivid/E2-TTS/E2TTS_Base/model_1200000.safetensors"))):
+    E2TTS_model_cfg = dict(dim=1024, depth=24, heads=16, ff_mult=4)
+    return load_model(UNetT, E2TTS_model_cfg, ckpt_path)
 
 
 def load_custom(ckpt_path: str, vocab_path="", model_cfg=None):
@@ -68,8 +76,8 @@ def load_custom(ckpt_path: str, vocab_path="", model_cfg=None):
 
 
 F5TTS_small_ema_model = load_f5tts_small()
-#F5TTS_ema_model = load_f5tts()
-#E2TTS_ema_model = load_e2tts() if USING_SPACES else None
+F5TTS_ema_model = load_f5tts()
+E2TTS_ema_model = load_e2tts() if USING_SPACES else None
 custom_ema_model, pre_custom_path = None, ""
 
 chat_model_state = None
@@ -108,8 +116,7 @@ def infer(
     indic = False
 
     if model == "F5-TTS":
-        indic = True
-        ema_model = F5TTS_small_ema_model
+        ema_model = F5TTS_ema_model
     elif model == "F5-TTS-small":
         indic = True
         ema_model = F5TTS_small_ema_model
